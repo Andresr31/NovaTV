eb636856c7402b4edba14aa51abc7cb07e989b6b
Carlos Andrés Rojas Parra <carlos.andres.1031@gmail.com>
2023-02-11 16:58:37 -0500

    Migrations finished

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->string('phone')->nullable();
+            $table->string('photo')->default('img/users/no-photo.png');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // Foreign Keys
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+
             $table->rememberToken();
             $table->timestamps();
         });
